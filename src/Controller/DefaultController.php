c84c59a576a6b6024bd82c1f94ecc7c223e60da7
Loore50 <Loore50@gmail.com>
2019-12-11 11:09:17 +0100

    costam

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,7 +12,7 @@ class DefaultController
  public function index()
  {
      $numberObject = new Stronka();
-
+    echo 1;
      echo $numberObject->number(2);
      die;
      return new Response('ok');
